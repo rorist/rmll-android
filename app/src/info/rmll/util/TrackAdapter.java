93430014b68c9f572c9f987bd7ba47228115b27e
Aubort Jean-Baptiste <aubort.jeanbaptiste@gmail.com>
2012-07-04 23:45:57 +0200

    Put back src...

---
+++ app/src/info/rmll/util/TrackAdapter.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package info.rmll.util;
+
+
+import info.rmll.pojo.Track;
+
+import java.util.ArrayList;
+
+import info.rmll.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * @author Christophe Vandeplas <christophe@vandeplas.com>
+ *
+ */
+public class TrackAdapter extends ArrayAdapter<Track> {
+	
+	private ArrayList<Track> items;
+
+	public TrackAdapter(Context context, int textViewResourceId, ArrayList<Track> items) {
+		super(context, textViewResourceId, items);
+		this.items = items;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+		if (v == null) {
+			v = LayoutInflater.from(getContext()).inflate(R.layout.track_list, null);
+		}
+
+		Track track = items.get(position);
+		if (track != null) {
+			TextView title = (TextView) v.findViewById(R.id.title);
+			title.setText(track.getName());
+			TextView type = (TextView) v.findViewById(R.id.type);
+			type.setText(track.getType());
+		}
+
+		return v;
+	}
+	
+}
